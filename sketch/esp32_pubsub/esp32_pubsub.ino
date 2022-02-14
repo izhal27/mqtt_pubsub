6e1c1b56dc205b0235ac9a67191b38a8ea33e15f
Risal Walangadi <risal.walangadi@gmail.com>
2022-02-14 16:57:34 +0800

    fix-beta

--- sketch/esp32_pubsub/esp32_pubsub.ino
+++ sketch/esp32_pubsub/esp32_pubsub.ino
@@ -2,8 +2,8 @@
 #include <PubSubClient.h>
 
 // WiFi
-const char *ssid = "Rumah Scarlett"; // Enter your WiFi name
-const char *password = "Scarlett27";  // Enter WiFi password
+const char *ssid = "MI8Lite"; // Enter your WiFi name
+const char *password = "walangadi27";  // Enter WiFi password
 
 // MQTT Broker
 const char *mqtt_broker = "broker.emqx.io";
@@ -12,14 +12,22 @@ const char *mqtt_username = "emqx";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
-const int pinLed = 27;
+const int pinRedLed = 27;
+const int pinYellowLed = 26;
+const int pinGreenLed = 25;
+unsigned long previousMillis = 0;
+unsigned long interval = 10000; // interval time
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
-  pinMode(pinLed, OUTPUT);
-  digitalWrite(pinLed, LOW);
+  pinMode(pinRedLed, OUTPUT);
+  pinMode(pinYellowLed, OUTPUT);
+  pinMode(pinGreenLed, OUTPUT);
+  digitalWrite(pinRedLed, HIGH);
+  digitalWrite(pinYellowLed, LOW);
+  digitalWrite(pinGreenLed, LOW);
 
   // Set software serial baud to 115200;
   Serial.begin(115200);
@@ -27,7 +35,7 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Connecting to " + String(( char *) ssid) + "...");
   }
   Serial.println("Connected to the WiFi network");
   //connecting to a mqtt broker
@@ -59,36 +67,65 @@ void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print((char) payload[i]);
   }
 
-  //  Serial.println();
   Serial.println();
   payload[length] = 0;
   String message = String(( char *) payload);
 
   //  Serial.println("Incomming message: " + message);
 
-  if (message == "ON") {
-    digitalWrite(pinLed, HIGH);
-  }
+  // expect message
+  // r=1;y=1;g=1 or r=0;y=1;g=1
+  int r = getValue(message, ';', 0).substring(2).toInt();
+  int y = getValue(message, ';', 1).substring(2).toInt();
+  int g = getValue(message, ';', 2).substring(2).toInt();
 
-  if (message == "OFF") {
-    digitalWrite(pinLed, LOW);
-  }
-
-  if (message == "status") {
-    Serial.println("publish message");
-    int mode = digitalRead(pinLed);
-
-    if (mode == 1) {
-      client.publish(topic, "ON");
-    } else {
-      client.publish(topic, "OFF");
-    }
-  }
+  digitalWrite(pinRedLed, r);
+  digitalWrite(pinYellowLed, y);
+  digitalWrite(pinGreenLed, g);
 
   Serial.println();
   Serial.println("-----------------------");
 }
 
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    Serial.println("publish message");
+
+    String redLedCurrent = String(digitalRead(pinRedLed));
+    String yellowLedCurrent = String(digitalRead(pinYellowLed));
+    String greenLedCurrent = String(digitalRead(pinGreenLed));
+
+    //    Serial.println("=================");
+    //    Serial.println(redLedCurrent);
+    //    Serial.println(yellowLedCurrent);
+    //    Serial.println(greenLedCurrent);
+    //    Serial.println("=================");
+
+    String statusLed = String("r=" + redLedCurrent + ";y=" + yellowLedCurrent + ";g=" + greenLedCurrent);
+    Serial.println(statusLed);
+
+    client.publish(topic, statusLed.c_str());
+  }
+
   client.loop();
 }
